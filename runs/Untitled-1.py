4540c7a68306630ad111c636fd598e6b3dc04ae4
MSPoulaei <sadeghpoolaee@gmail.com>
2025-02-21 12:34:04 +0330

    add project files

---
+++ runs/Untitled-1.py
@@ -0,0 +1,39 @@
+langs=["amh","arq","ary","chn","deu","esp","hau","hin","ibo","kin","mar","orm","pcm","ptbr","ptmz","ron","rus","som","sun","swa","swe","tat","tir","ukr","vmw","yor","ind","jav","xho","zul"]
+langs2country={
+    "afr":"Afrikaans",
+    "amh":"Amharic",
+    "arq":"Algerian Arabic",
+    "ary":"Moroccan Arabic",
+    "chn":"Chinese",
+    "deu":"German",
+    "esp":"Latin American Spanish",
+    "eng":"English",
+    "hau":"Hausa",
+    "hin":"Hindi",
+    "ibo":"Igbo",
+    "kin":"Kinyarwanda",
+    
+    "mar":"Marathi",
+    "orm":"Oromo",
+    "pcm":"Nigerian-Pidgin",
+    "ptbr":"Brazilian Portuguese",
+    "ptmz":"Mozambican Portuguese",
+    "ron":"Romanian",
+    "rus":"Russian",
+    "som":"Somali",
+    "sun":"",#Sundanese
+    "swa":"Swahili",
+    "swe":"Swedish",
+    "tat":"Tatar",
+    "tir":"Tigrinya",
+    "ukr":"Ukrainian",
+    "vmw":"",#Makhuwa
+    "yor":"Yoruba",
+    "ind":"Indonesian",
+    "jav":"Javanese",
+    "xho":"isiXhosa",
+    "zul":"isiZulu Arabic",
+}
+"""
+Setswana, Xitsonga
+"""
